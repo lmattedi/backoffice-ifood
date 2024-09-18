0ac26fb054f6c097cd97453eb73b1ae480dc84ae
Lucian Mattedi <62583096+lmattedi@users.noreply.github.com>
2024-09-18 19:50:21 -0300

    Delete gestao.js

--- gestao.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Hamburger menu toggle functionality
-    const menuToggle = document.getElementById('menu-toggle');
-    const wrapper = document.getElementById('wrapper');
-
-    menuToggle.addEventListener('click', function () {
-        wrapper.classList.toggle('toggled');
-    });
-});
